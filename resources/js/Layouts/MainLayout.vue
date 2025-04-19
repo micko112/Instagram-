b7f7d47348b05e97d782c795869bd21f8e582472
micko112 <dimitrijemitic112@gmail.com>
2025-04-19 21:20:11 +0200

    Prvi komit, obrisao sam samo nepotrebne delove i radim na Home-u i main layoutu

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+import { Link } from '@inertiajs/vue3';
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import Compass from 'vue-material-design-icons/Compass.vue';
+import SendOutLine from 'vue-material-design-icons/SendOutLine.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import AccountOutLine from 'vue-material-design-icons/AccountOutLine.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import AccountPlusOutLine from 'vue-material-design-icons/AccountPlusOutLine.vue';
+
+let showCreatePost = ref(false);
+</script>
+
+<template>
+
+    <div id="MainLayout" class="bg-red-600">
+        <div>MainLayout</div>
+    </div>
+    <slot/>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<template>
+<head title="HOME"/>
+    <MainLayout>
+        <div class="bg-green-500">HOME.VUE</div>
+    </MainLayout>
+
+</template>
